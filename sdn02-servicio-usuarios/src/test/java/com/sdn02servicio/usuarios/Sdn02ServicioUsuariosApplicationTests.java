a783d0015a69fdf28234b79267e23398bf510e63
Nahum Martinez <nmartinez.salgado@yahoo.com>
2021-12-07 18:47:15 -0600

    NAM-1

---
+++ sdn02-servicio-usuarios/src/test/java/com/sdn02servicio/usuarios/Sdn02ServicioUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdn02servicio.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdn02ServicioUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

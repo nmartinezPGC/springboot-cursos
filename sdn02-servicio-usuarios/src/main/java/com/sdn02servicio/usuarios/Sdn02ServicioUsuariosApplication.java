7cad7cd61b4a5ffd052541d5d47565b1a8c7158f
Nahum Martinez <nmartinez.salgado@yahoo.com>
2021-12-07 18:16:35 -0600

    Commit Inicial

---
+++ sdn02-servicio-usuarios/src/main/java/com/sdn02servicio/usuarios/Sdn02ServicioUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.sdn02servicio.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdn02ServicioUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdn02ServicioUsuariosApplication.class, args);
+	}
+
+}
